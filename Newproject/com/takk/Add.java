cf771a86503a891f2fae6631aea25cf20d2bae11
Admin <stpatil171001@gmail.com>
2024-12-27 12:06:51 +0530

    New Changes

--- Newproject/com/takk/Add.java
+++ Newproject/com/takk/Add.java
@@ -4,7 +4,8 @@ public class Add {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println(10+10    );
+System.out.println(10+10);
+System.out.println(30+20);
 	}
 
 }
